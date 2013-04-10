093e224d670cc7e7d3009cbf8ae781e5aa319622
Kevin Kirsche <kkirsche@hsc.edu>
2013-04-10 15:59:53 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,128 @@
+<?php require_once "model/RSSFeed.php"; ?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <style>
+            body {
+                padding-top: 60px;
+                padding-bottom: 40px;
+            }
+        </style>
+        <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/?locale=en">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    <a class="brand" href="#">Charcoal</a>
+                    <div class="nav-collapse collapse">
+                        <!--<form class="navbar-form pull-right">
+                            <input class="span2" type="text" placeholder="Email">
+                            <input class="span2" type="password" placeholder="Password">
+                            <button type="submit" class="btn">Sign in</button>
+                        </form>-->
+                    </div><!--/.nav-collapse -->
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="span3">
+                    <ul class="nav nav-list">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li>
+                            <div class="btn-group">
+                                <button class="btn dropdown-toggle" data-toggle="dropdown">Subscribe <span class="caret"></span></button>
+                                <ul class="dropdown-menu">
+                                    <li>Something</li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="divider"></li>
+                        <li class="nav-header">All Items</li>
+                            <li><a href="#"><i class="icon-star"></i> Starred items</a></li>
+                            <li><a href="#"><i class="icon-fire"></i> Trends</a></li>
+                        <li class="divider"></li>
+                        <li class="nav-header">Subscriptions</li>
+                        <ul class="nav nav-list">
+                            <li><a href="#" data-toggle="collapse" data-target="#Apple"><i class="icon-folder-open folderIcon"></i> Apple</a></li>
+                            <div class="collapse in" id="Apple">
+                                <ul class="nav nav-list">
+                                    <li><a href="#"><img src="img/exampleFiles/cultOfMac.png" alt="Cult of Mac" /> Cult of Mac</a></li>
+                                    <li><a href="#">Macworld</a></li>
+                                    <li><a href="#">iPad.AppStorm</a></li>
+                                    <li><a href="#">iPhone.AppStorm</a></li>
+                                    <li><a href="#">TUAW</a></li>
+                                    <li><a href="#">Mac.AppStorm</a></li>
+                                    <li><a href="#">9to5Mac</a></li>
+                                    <li><a href="#">MacRumors</a></li>
+                                </ul>
+                            </div>
+                            <li><a href="#"><i class="icon-folder-close"></i> AskMen</a></li>
+                            <li><a href="#"><i class="icon-folder-close"></i> Dance Music</a></li>
+                            <li><a href="#"><i class="icon-folder-close"></i> Design &amp; Development</a></li>
+                            <li><a href="#"><i class="icon-folder-close"></i> iOS Development</a></li>
+                            <li><a href="#"><i class="icon-folder-close"></i> Leadership</a></li>
+                            <li><a href="#"><i class="icon-folder-close"></i> Learning</a></li>
+                            <li><a href="#"><i class="icon-folder-close"></i> Video Games</a></li>
+                        </ul>
+                    </ul>
+                </div><!--/div.span3-->
+                <div class="span9">
+                    <article>
+                        <?php 
+                            $RSSFeed = new RSSFeed();
+                            $RSSFeed->getFeed("http://feedproxy.google.com/nettuts") 
+                        ?>
+                    </article>
+                    <hr />
+                </div>
+            </div><!--/div.row-->
+            <hr />
+            <footer>
+                <p>&copy; Kevin Kirsche 2012&ndash;<?php echo date('Y'); ?> </p>
+            </footer>
+
+        </div> <!-- /container -->
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+        <script>
+            $(".collapse").collapse();
+        </script>
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
---
+++ model/RSSFeed.php
@@ -0,0 +1,58 @@
+<?php
+    class RSSFeed {
+
+        private function rssToTime($rss_time) {
+                $day = substr($rss_time, 5, 2);
+                $month = substr($rss_time, 8, 3);
+                $month = date('m', strtotime("$month 1 2011"));
+                $year = substr($rss_time, 12, 4);
+                $hour = substr($rss_time, 17, 2);
+                $min = substr($rss_time, 20, 2);
+                $second = substr($rss_time, 23, 2);
+                $timezone = substr($rss_time, 26);
+
+                $timestamp = mktime($hour, $min, $second, $month, $day, $year);
+
+                date_default_timezone_set('America/New_York');
+
+                if(is_numeric($timezone)) {
+                    $hours_mod = $mins_mod = 0;
+                    $modifier = substr($timezone, 0, 1);
+                    $hours_mod = (int) substr($timezone, 1, 2);
+                    $mins_mod = (int) substr($timezone, 3, 2);
+                    $hour_label = $hours_mod>1 ? 'hours' : 'hour';
+                    $strtotimearg = $modifier.$hours_mod.' '.$hour_label;
+                    if($mins_mod) {
+                        $mins_label = $mins_mod>1 ? 'minutes' : 'minute';
+                        $strtotimearg .= ' '.$mins_mod.' '.$mins_label;
+                    }
+                    $timestamp = strtotime($strtotimearg, $timestamp);
+                }
+
+                return date("D, F d, Y", $timestamp);
+        }
+
+        public function getFeed($feed_url) {
+            $content = file_get_contents($feed_url);
+            $xml = new SimpleXmlElement($content);
+
+            foreach($xml->channel->item as $entry) {
+                $output = "";
+                //get everything ready
+                $entry->shortDescription = substr($entry->description, 0, strpos($entry->description," ",1500));
+                //send it to the browser
+                $output .= "<article>";
+                    $output .= "<h3><a href=\"$entry->link\" title=\"$entry->title\" class=\"articleTitle\">" . $entry->title . "</a></h3>";
+                    if ($entry->category) {
+                        $output .= "<p><strong>Category:</strong> " . $entry->category . " &bull; <strong>Date:</strong> " . $this->rssToTime($entry->pubDate) . "</p>";
+                    }
+                    $output .= $entry->shortDescription;
+                    $output .= "<br />";
+                    $output .= "<a title=\"Read More\" class=\"btn btn-info\">Read More</a>";
+                $output .= "</article><hr />";
+
+                echo $output;
+            }
+        }
+    }
+?>
