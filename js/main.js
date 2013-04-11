eb5b963aa4171fff976d0888f60d9e74759f30af
Kevin Kirsche <kkirsche@hsc.edu>
2013-04-11 13:44:05 -0400

    Cross domain ajax

--- js/main.js
+++ js/main.js
@@ -9,9 +9,10 @@ $(document).ready(function() {
         $.ajax('http://kirsches.us/Charcoal/model/RSSFeed.php', {
             data: {url: feedURL},
             beforeSend: function() {
-                
+                $("#feedBody").html("<div class=\"alert alert-info span7\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button><strong>Loading Feed&hellip;</strong>");
             },
             cache: false,
+            dataType: 'jsonp',
             success: function(result) {
                 $("#feedBody").html(result);
             },
